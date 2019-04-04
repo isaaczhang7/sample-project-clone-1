a4ebdfbf80377bbdfda43e79cb621345e9c29f86
Isaac Zhang <isaaczhang@ibm.com>
2019-04-04 14:32:03 -0700

    Adding xcode project

---
+++ Sample-project-1/Sample-project-1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sample-project-1
+//
+//  Created by Isaac Zhang on 4/4/19.
+//  Copyright © 2019 Isaac Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
